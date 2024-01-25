2cf899e067db2341de43cfe11b5243b7a528fb14
bmqy <bmqy@qq.com>
2024-01-25 13:26:11 +0800

    1.0.0

--- usr/themes/simple/footer.php
+++ usr/themes/simple/footer.php
@@ -1,6 +1,6 @@
 <?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
 
-<footer id="footer" role="contentinfo">
+<footer id="footer" class="py-10 px-3.5" role="contentinfo">
     &copy; <?php echo date('Y'); ?> <a href="<?php $this->options->siteUrl(); ?>"><?php $this->options->title(); ?></a>.<br/>
     <?php _e('由 <a href="https://typecho.org">Typecho</a> 强力驱动，<a href="https://github.com/bmqy/Typecho-Simple-Mibiao">Typecho-Simple-Mibiao</a> 提供支持'); ?>.
     <?php if($this->options->cpsInfo !=''): ?>
